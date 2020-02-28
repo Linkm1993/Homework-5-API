8e2aac1d8f730d6c6bf070ceed1ca0dd352b2754
Matt Link <Linkm1993@gmail.com>
2020-02-28 18:16:42 -0500

    changed styling

--- js/script.js
+++ js/script.js
@@ -4,16 +4,29 @@ let timeOfDay = ["6 AM","7 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM
 let currentTime = moment()
 
 
-for (i = 0; i < timeOfDay.length; i++){
-    let timeArray = []
-    let dayDiv = home.append("<div class=\"time\">")
-    let brkDiv = home.append("<br>")
 
-    timeArray.push(timeOfDay[i])
-    dayDiv.append(timeArray)
+for (i = 0; i < timeOfDay.length; i++){
+    let timeArray = [];
+    let dayDiv = home.append("<div class=\"time\">");
+
+    timeArray.push(timeOfDay[i]);
+    dayDiv.append(timeArray);
 
 
 }
+
+
+function storeLocal(input) {
+    let emptyString ="A"
+    let userInput = $(".Submitform-control-md")
+    let submitButton = $(".btn")
+    userInput.val(45)
+    console.log(userInput.val())
+
+console.log(emptyString)
+}
+
+
 console.log(currentTime)
 console.log(currentTime.toString())
 console.log(currentTime._locale._months)
